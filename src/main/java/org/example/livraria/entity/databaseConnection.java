ee146978b5c50c968174a2b2e452124599aec759
UriasDR <129330378+UriasDR@users.noreply.github.com>
2023-05-23 21:56:36 -0300

    Add files via upload

---
+++ src/main/java/org/example/livraria/entity/databaseConnection.java
@@ -0,0 +1,10 @@
+package org.example.livraria.entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class databaseConnection {
+    String url = "jdbc:mysql://localhost:3306/livraria";
+    String username = "root";
+    String password = "admin123";
+}
